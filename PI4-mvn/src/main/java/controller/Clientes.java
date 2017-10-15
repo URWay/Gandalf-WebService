53ece5576976f0e8c5eb493a30fe6bea1413923e
jeffersonSantos13 <yuiti.santos@gmail.com>
2017-10-14 22:37:06 -0300

    Web Service Cliente
    Finalizado!

--- PI4-mvn/src/main/java/controller/Clientes.java
+++ PI4-mvn/src/main/java/controller/Clientes.java
@@ -4,7 +4,6 @@ import modelos.Conexao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -17,15 +16,15 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 
 import modelos.Cliente;
-
+import org.codehaus.jettison.json.JSONObject;
 
 @Path("/cliente")
 public class Clientes {
     
     @GET
-    @Path("/{idCliente}")
+    @Path("/{param}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getCliente(@PathParam("idCliente") String idCliente) throws Exception{
+    public Response getCliente(@PathParam("param") String idCliente) throws Exception{
         
         String id = idCliente;
         Cliente c = null;
@@ -48,7 +47,7 @@ public class Clientes {
                     c.setTelComercialCliente(rs.getString("telComercialCliente"));
                     c.setTelResidencialCliente(rs.getString("telResidencialCliente"));
                     c.setDtNascCliente(rs.getString("dtNascCliente"));
-                    c.setRecebeNewsLetter(rs.getString("receberNewsLetter"));
+                    c.setRecebeNewsLetter(rs.getInt("recebeNewsLetter"));
                 }
             }   
             
@@ -57,7 +56,7 @@ public class Clientes {
         }
         
         if(c == null) {
-            return Response.status(404).entity(c).build();
+            return Response.status(406).entity(c).build();
         } else {
             return Response.status(200).entity(c).build();
         }
@@ -66,130 +65,121 @@ public class Clientes {
     @POST
     @Path("/inserir")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response inserirUsuario(List<Cliente> c){
+    public Response inserirUsuario(String content) throws Exception{
+        JSONObject object = new JSONObject(content);
         
-        if(c.size() > 0) {
-           // Gson gson = new Gson();
-          //  String jsonCliente = gson.toJson(c.get(0));
-          //  
-            // Recebendo os dados via Json e adicionando na Classe Cliente
-            //Cliente cliente = gson.fromJson(jsonCliente, Cliente.class);
+        if (!object.isNull(content)){
+            return Response.status(400).build();
+        }
+        
+        String sql = "INSERT INTO cliente ("
+                + "nomeCompletoCliente, "
+                + "emailCliente, "
+                + "senhaCliente, "
+                + "CPFCliente, "
+                + "celularCliente, "
+                + "telComercialCliente, "
+                + "telResidencialCliente, "
+                + "dtNascCliente, "
+                + "recebeNewsLetter) "
+                + "VALUES(?,?,?,?,?,?,?,?,?)";
+        
+        try {
+            Connection con = Conexao.get().conn();
 
-            String sql = "INSERT INTO cliente ("
-                    + "nomeCompletoCliente, "
-                    + "emailCliente, "
-                    + "senhaCliente,"
-                    + "CPFCliente,"
-                    + "celularCliente,"
-                    + "telComercialCliente,"
-                    + "telResidencialCliente,"
-                    + "dtNascCliente,"
-                    + "receberNewsLetter)"
-                    + "VALUES(?,?,?,?,?,?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, object.getString("nomeCompletoCliente"));
+            ps.setString(2, object.getString("emailCliente")); 
+            ps.setString(3, object.getString("senhaCliente"));
+            ps.setString(4, object.getString("CPFCliente"));
+            ps.setString(5, object.getString("celularCliente"));
+            ps.setString(6, object.getString("telComercialCliente"));
+            ps.setString(7, object.getString("telResidencialCliente"));
+            ps.setString(8, object.getString("dtNascCliente"));
+            ps.setInt(9, object.getInt("recebeNewsLetter"));
 
-            try {
-                Connection con = Conexao.get().conn();
-                
-                PreparedStatement ps = con.prepareStatement(sql);
-         //       ps.setString(1, cliente.getNomeCompletoCliente());
-           //     ps.setString(2, cliente.getEmailCliente()); 
-             //   ps.setString(3, cliente.getSenhaCliente());
-          //      ps.setString(4, cliente.getCPFCliente());
-           //     ps.setString(5, cliente.getCelularCliente());
-            //    ps.setString(6, cliente.getTelComercialCliente());
-             //   ps.setString(7, cliente.getTelResidencialCliente());
-              //  ps.setString(8, cliente.getDtNascCliente());
-              //  ps.setString(9, cliente.getRecebeNewsLetter());
-
-                if(ps.executeUpdate() > 0){
-                   return Response.status(200).entity("").build();
-                } else {
-                    return Response.status(406).entity("").build();
-                }
-
-            } catch(Exception ex){
-                return Response.status(500).entity(null).build();
+            if(ps.executeUpdate() > 0){
+               return Response.ok().build();
+            } else {
+                return Response.status(404).build();
             }
 
-        } else {
-            return Response.status(406).entity("").build();
-        }
+        } catch(Exception ex){
+            return Response.status(500).entity(null).build();
+        }       
     }
     
     @PUT
     @Path("/atualizar")
     @Consumes(MediaType.APPLICATION_JSON) 
-    public Response alterarUsuario(Cliente cliente){
+    public Response alterarUsuario(String content) throws Exception{
+        JSONObject object = new JSONObject(content);
         
-        if(cliente.getIdCliente() > 0) {
-            
-            String sql = "UPDATE Cliente set " 
-                    + "nomeCompletoCliente = ?, "
-                    + "emailCliente = ?, "
-                    + "senhaCliente = ?,"
-                    + "CPFCliente = ? ,"
-                    + "celularCliente = ? ,"
-                    + "telComercialCliente = ? ,"
-                    + "telResidencialCliente = ? ,"
-                    + "dtNascCliente = ? ,"
-                    + "receberNewsLetter = ?"
-                    + "WHERE idCliente = ?"; 
-            
-            try {
-                Connection con = Conexao.get().conn();
-                
-                PreparedStatement ps = con.prepareStatement(sql);
-                ps.setString(1, cliente.getNomeCompletoCliente());
-                ps.setString(2, cliente.getEmailCliente()); 
-                ps.setString(3, cliente.getSenhaCliente());
-                ps.setString(4, cliente.getCPFCliente());
-                ps.setString(5, cliente.getCelularCliente());
-                ps.setString(6, cliente.getTelComercialCliente());
-                ps.setString(7, cliente.getTelResidencialCliente());
-                ps.setString(8, cliente.getDtNascCliente());
-                ps.setString(9, cliente.getRecebeNewsLetter());
-                ps.setInt(10, cliente.getIdCliente());
-
-                if(ps.executeUpdate() > 0){
-                    return Response.status(200).entity(cliente).build();
-                } else {
-                    return Response.status(406).entity(cliente).build();
-                }
-                
-            } catch(Exception ex){
-                return Response.status(500).entity(null).build();
-            }
-            
-        } else {
-            return Response.status(406).entity("").build();
+        if (!object.isNull(content)){
+            return Response.status(400).build();
         }
-        
+            
+        String sql = "UPDATE Cliente SET " 
+                + "nomeCompletoCliente = ?, "
+                + "emailCliente = ?, "
+                + "senhaCliente = ?, "
+                + "CPFCliente = ?, "
+                + "celularCliente = ?, "
+                + "telComercialCliente = ?, "
+                + "telResidencialCliente = ?, "
+                + "dtNascCliente = ?, "
+                + "recebeNewsLetter = ? "
+                + "WHERE idCliente = ?"; 
+
+        try {
+            Connection con = Conexao.get().conn();
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, object.getString("nomeCompletoCliente"));
+            ps.setString(2, object.getString("emailCliente")); 
+            ps.setString(3, object.getString("senhaCliente"));
+            ps.setString(4, object.getString("CPFCliente"));
+            ps.setString(5, object.getString("celularCliente"));
+            ps.setString(6, object.getString("telComercialCliente"));
+            ps.setString(7, object.getString("telResidencialCliente"));
+            ps.setString(8, object.getString("dtNascCliente"));
+            ps.setInt(9, object.getInt("recebeNewsLetter"));
+            ps.setInt(10, object.getInt("idCliente"));
+
+            if(ps.executeUpdate() > 0){
+                return Response.ok().build();
+            } else {
+                return Response.status(404).build();
+            }
+
+        } catch(Exception ex){
+            return Response.status(500).entity(null).build();
+        }
     }
     
     @DELETE
-    @Path("/deletar")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response deleteCliente(Cliente cliente){
+    @Path("/deletar/{param}")
+    public Response deleteCliente(@PathParam("param") String idCliente){
         
-        if (cliente.getIdCliente() > 0) {
+        if (!idCliente.equals("")) {
             String sql = "DELETE FROM Cliente WHERE idCliente = ?";
             
             try {
                 Connection con = Conexao.get().conn();    
                 PreparedStatement ps = con.prepareStatement(sql);
-                ps.setInt(1, cliente.getIdCliente());
+                ps.setInt(1, Integer.parseInt(idCliente));
                 
                 if(ps.executeUpdate() > 0){
-                    return Response.status(200).entity(cliente).build();
+                    return Response.ok().build();
                 } else {
-                    return Response.status(406).entity("").build();
+                    return Response.status(404).build();
                 }
                 
             } catch(Exception ex){
                 return Response.status(500).entity(null).build();
             }
         } else {
-            return Response.status(406).entity("").build();
+            return Response.status(406).build();
         }
     }
 }
