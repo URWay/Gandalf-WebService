e3e091830917d82d47de318ad67cc014b9a9da0b
augustoaraujo97 <augustoaraujo.97@gmail.com>
2017-11-01 20:34:46 -0200

    pedidos

---
+++ PI4-mvn/src/main/java/modelos/PedidosItens.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author augusto.naraujo
+ */
+public class PedidosItens {
+    private int idPedido;
+    private int idCliente;
+    private int idStatus;
+    private String dataPedido;
+    private int idTipoPagto;
+    private int idEndereco;
+    private int idAplicacao;
+    private int idProduto;
+    private double qtdProduto;
+    private double precoVendaItem;
+    private String nomeProduto;
+    private String descProduto;
+    private double precProduto;
+    private double descontoPromocao;
+    private int idCategoria;
+    private String ativoProduto;
+    private int idUsuario;
+    private int qtdMinEstoque;
+    private String imagem;
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdStatus() {
+        return idStatus;
+    }
+
+    public void setIdStatus(int idStatus) {
+        this.idStatus = idStatus;
+    }
+
+    public String getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(String dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public int getIdTipoPagto() {
+        return idTipoPagto;
+    }
+
+    public void setIdTipoPagto(int idTipoPagto) {
+        this.idTipoPagto = idTipoPagto;
+    }
+
+    public int getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(int idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public int getIdAplicacao() {
+        return idAplicacao;
+    }
+
+    public void setIdAplicacao(int idAplicacao) {
+        this.idAplicacao = idAplicacao;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public double getQtdProduto() {
+        return qtdProduto;
+    }
+
+    public void setQtdProduto(double qtdProduto) {
+        this.qtdProduto = qtdProduto;
+    }
+
+    public double getPrecoVendaItem() {
+        return precoVendaItem;
+    }
+
+    public void setPrecoVendaItem(double precoVendaItem) {
+        this.precoVendaItem = precoVendaItem;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getDescProduto() {
+        return descProduto;
+    }
+
+    public void setDescProduto(String descProduto) {
+        this.descProduto = descProduto;
+    }
+
+    public double getPrecProduto() {
+        return precProduto;
+    }
+
+    public void setPrecProduto(double precProduto) {
+        this.precProduto = precProduto;
+    }
+
+    public double getDescontoPromocao() {
+        return descontoPromocao;
+    }
+
+    public void setDescontoPromocao(double descontoPromocao) {
+        this.descontoPromocao = descontoPromocao;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getAtivoProduto() {
+        return ativoProduto;
+    }
+
+    public void setAtivoProduto(String ativoProduto) {
+        this.ativoProduto = ativoProduto;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getQtdMinEstoque() {
+        return qtdMinEstoque;
+    }
+
+    public void setQtdMinEstoque(int qtdMinEstoque) {
+        this.qtdMinEstoque = qtdMinEstoque;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+    
+    
+    
+}
